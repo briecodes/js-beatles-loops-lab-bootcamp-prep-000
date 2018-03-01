a81dab841439af8f906911b21a0a630df8c861d5
Brian A Morales <bombibills@gmail.com>
2018-03-01 16:42:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for (var i = 0; i < musicians.length; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return newArray;
+}
+
+// function johnLennonFacts(johnFacts){
+//   var i = 0;
+//   do{
+//     console.log(`Round: ${i}`);
+//     johnFacts[i] = `${johnFacts[i]}!!!`;
+//     console.log(`Round2: ${i}`);
+//     // return johnFacts;
+//   } while( i < johnFacts.length && johnLennonFacts());
+// }
+
+function johnLennonFacts(johnFacts){
+  for (var i = 0; ; i++){
+    var myNumber = i;
+    console.log("my number: " + myNumber);
+    break;
+  }
+  do{
+    johnFacts[i] = `${johnFacts}!!!`;
+    console.log(johnFacts);
+  } while (i < johnFacts.length && johnLennonFacts());
+}
